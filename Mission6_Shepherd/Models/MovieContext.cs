965a158384c8601a86fefa6eb778e52c8c74c498
mshep8 <marycatherineshepherd@gmail.com>
2026-02-10 08:20:53 -0700

    comments

--- Mission6_Shepherd/Models/MovieContext.cs
+++ Mission6_Shepherd/Models/MovieContext.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 
+// This namespace groups related model/form files together
 namespace Mission6_Shepherd.Models.Forms;
 
+// This class represents the database connection for Movies
 public class MovieContext : DbContext
 {
+    // Constructor: passes database options to the base DbContext class
     public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
 
+    // This DbSet represents the Movies table in the database
     public DbSet<Movie> Movies { get; set; } = null!;
 }
